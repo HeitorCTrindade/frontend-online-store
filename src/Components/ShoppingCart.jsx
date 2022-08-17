2ee9fed77c641837d2c131aab1f2d430cbaf5ec9
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-08-17 01:00:11 -0300

    Requisito 10 - implementado

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -27,18 +27,76 @@ export default class ShoppingCart extends Component {
           <p data-testid="shopping-cart-product-name">{ product.title }</p>
           <img src={ product.thumbnail } alt="" />
           <p>{product.price}</p>
-          <p data-testid="shopping-cart-product-quantity">
+          <button
+            type="button"
+            name="decrease-button"
+            data-testid="product-decrease-quantity"
+            onClick={ (event) => this.handleQuantityProductsChanges(event, product) }
+          >
+            -
+          </button>
+          <p id="quantity" data-testid="shopping-cart-product-quantity">
             { this.checkQuantityProductsTypeInCart(product) }
           </p>
+          <button
+            type="button"
+            name="increase-button"
+            data-testid="product-increase-quantity"
+            onClick={ (event) => this.handleQuantityProductsChanges(event, product) }
+          >
+            +
+          </button>
+          <button
+            type="button"
+            name="remove-button"
+            data-testid="remove-product"
+            onClick={ (event) => this.handleQuantityProductsChanges(event, product) }
+          >
+            remover produto
+          </button>
         </div>
       ))
     );
   }
 
+  handleLocalStorageUpdates = (cartArrayToSave) => {
+    localStorage.setItem('cartItens', JSON.stringify(cartArrayToSave));
+  }
+
+  handleQuantityProductsChanges = (event, productToCheck) => {
+    const { arrayProducts } = this.state;
+    if (event.target.name === 'remove-button') {
+      const tempArrayProducts = arrayProducts
+        .filter((product) => product.id !== productToCheck.id);
+      this.setState({ arrayProducts: tempArrayProducts });
+      this.handleLocalStorageUpdates(tempArrayProducts);
+    }
+    if (event.target.name === 'increase-button') {
+      const tempArrayProducts = arrayProducts.map((product) => {
+        if (product.id === productToCheck.id) {
+          product.quantity += 1;
+        }
+        return product;
+      });
+      this.setState({ arrayProducts: tempArrayProducts });
+      this.handleLocalStorageUpdates(tempArrayProducts);
+    }
+    if (event.target.name === 'decrease-button') {
+      const tempArrayProducts = arrayProducts.map((product) => {
+        if (product.id === productToCheck.id && product.quantity !== 1) {
+          product.quantity -= 1;
+        }
+        return product;
+      });
+      this.setState({ arrayProducts: tempArrayProducts });
+      this.handleLocalStorageUpdates(tempArrayProducts);
+    }
+  }
+
   checkQuantityProductsTypeInCart = (productToCheck) => {
     const { arrayProducts } = this.state;
     return arrayProducts.filter((product) => product.id
-    === productToCheck.id).length;
+    === productToCheck.id)[0].quantity;
   }
 
   render() {
